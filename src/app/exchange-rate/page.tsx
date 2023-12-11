6fd7bca566777161b17586670336db093ef66172
JonathanSoto <61751017+Bushogun@users.noreply.github.com>
2023-12-11 10:30:12 -0500

    feat: :sparkles: pagination implemented

--- src/app/exchange-rate/page.tsx
+++ src/app/exchange-rate/page.tsx
@@ -3,7 +3,9 @@ import React, { useEffect } from 'react';
 import styles from '@/app/exchange-rate/exchange-rate.module.scss';
 import CryptoCard from '@/app/components/crypto-card';
 import { useAppSelector, useAppDispatch } from '@/redux/hooks';
-import  LoadingSpinner  from '@/app/components/loading-spinner/loading-spinner';
+import { usePagination } from '@/app/components/pagination/pagination';
+import Pagination from './list-pagination';
+import LoadingSpinner from '@/app/components/loading-spinner/loading-spinner';
 import { SearchBarForm } from '@/app/components/search-bar/search-bar-form';
 import { setCryptos, setError, setLoading } from '@/redux/features/crypto-slice';
 import { ICrypto } from '@/app/interfaces/i-crypto';
@@ -14,9 +16,21 @@ const ExchangeRate = () => {
   const filterQuery = useAppSelector((state) => state.currencyReducer.filterQuery);
   const loading = useAppSelector((state) => state.currencyReducer.loading);
   const error = useAppSelector((state) => state.currencyReducer.error);
+  const itemsPerPage = useAppSelector((state) => state.currencyReducer.itemsPerPage);
   const apiUrl = process.env.NEXT_PUBLIC_API_URL!;
   const requestCryptos = process.env.NEXT_PUBLIC_REQUEST_CRYPTOS!;
   const endPoint = apiUrl + requestCryptos;
+  const allCryptos = cryptos || [];
+
+  const filteredCryptos = allCryptos.filter((crypto: ICrypto) =>
+    crypto.name.toLowerCase().includes(filterQuery.toLowerCase()) ||
+    crypto.symbol.toLowerCase().includes(filterQuery.toLowerCase())
+  );
+
+  const { currentPage, paginate, currentItems, totalPages } = usePagination(
+    itemsPerPage,
+    filteredCryptos
+  );
 
   useEffect(() => {
     dispatch(setLoading(true));
@@ -31,33 +45,33 @@ const ExchangeRate = () => {
       );
   }, []);
 
-  const filteredCryptos = cryptos?.filter((crypto: ICrypto) => {
-    return (
-      crypto.name.toLowerCase().includes(filterQuery.toLowerCase()) ||
-      crypto.symbol.toLowerCase().includes(filterQuery.toLowerCase())
-    );
-  });
-
   return (
-    <>
-      {loading ? (
-        <LoadingSpinner />
-      ) : error ? (
-        <p>Error: {error}</p>
-      ) : (
-        <div className={styles['container-page']}>
-          <div className={styles['container-cards']}>
-            <h1>Exchange Rate</h1>
-            <div className={styles['container-search-bar']}>
-              <SearchBarForm />
+    <div className={styles.listContainer}>
+      <>
+        {loading ? (
+          <LoadingSpinner />
+        ) : error ? (
+          <p>Error: {error}</p>
+        ) : (
+          <div className={styles['container-page']}>
+            <div className={styles['container-cards']}>
+              <h1>Exchange Rate</h1>
+              <div className={styles['container-search-bar']}>
+                <SearchBarForm />
+              </div>
+              {currentItems().map((crypto: ICrypto) => (
+                <CryptoCard key={crypto.id} crypto={crypto} />
+              ))}
+              <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={paginate}
+              />
             </div>
-            {filteredCryptos?.map((crypto: ICrypto) => (
-              <CryptoCard key={crypto.id} crypto={crypto} />
-            ))}
           </div>
-        </div>
-      )}
-    </>
+        )}
+      </>
+    </div>
   );
 };
 
